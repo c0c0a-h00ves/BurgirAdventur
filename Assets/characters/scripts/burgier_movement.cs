0ad5d2f419c01d89b62421646f8f0b22e73a6db8
c0c0a-h00ves <99198202+c0c0a-h00ves@users.noreply.github.com>
2022-02-09 17:15:04 +0100

    Cleaning burgir)movement and merging it with burgir_jumping

--- Assets/characters/scripts/burgier_movement.cs
+++ Assets/characters/scripts/burgier_movement.cs
@@ -6,26 +6,43 @@ public class burgier_movement : MonoBehaviour
     float horizontal_direction = 0f;
     float horizontal_move = 0f;
     Vector3 complete_move;
-    bool isAllowed_right = true;
-    bool isAllowed_left = true;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
 
 // Zmienne ktore modyfikujemy my (w przyszlosci do beda stale)
 // serialize daje ze mozna zmieniac zmienna w komponencie skryptu
     [SerializeField] float speed = 1f;
+    [SerializeField] float jumpVelocity = 3f;
+    [SerializeField] private LayerMask platformslayerMask;
+
+    private void Awake()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+    }
 
     void Update(){
         horizontal_direction = Input.GetAxisRaw("Horizontal");
+        
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
     }
 
     void FixedUpdate(){
-        Vector3 pozycja_temp = transform.position;
-
-        horizontal_move = horizontal_direction * speed;
-      
+        //mnożąc kierunek przez prędkość uzyskujemy ruch horyzontalny jaki chcemy wykonać       
+        horizontal_move = horizontal_direction * speed;  
         complete_move = new Vector2(horizontal_move, 0f);
-        GetComponent<Rigidbody2D>().AddForce(complete_move );
+        rigidbody2d.AddForce(complete_move);
     
-    
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 0.1f, platformslayerMask);
+        Debug.Log(raycastHit2d);
+        return raycastHit2d.collider != null;
     }
 
 }
