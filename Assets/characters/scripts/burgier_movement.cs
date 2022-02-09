2c45f8b4e8bb0644c037077c3125928a6478da0c
Dimrondvv <dominik.glowinski2@gmail.com>
2022-02-09 21:06:15 +0100

    Add Better Jump like Mario
    It's me Mario Łaaaaaa

--- Assets/characters/scripts/burgier_movement.cs
+++ Assets/characters/scripts/burgier_movement.cs
@@ -2,25 +2,51 @@ using UnityEngine;
 
 public class burgier_movement : MonoBehaviour
 {
-    [Header("Horizontal Movment")]
+    [Header("Horizontal Movement")]
     public float moveSpeed = 10f;
     public Vector2 direction;
 
+    [Header("Vertical Movement")]
+    public float jumpSpeed = 15f;
+    public float jumpDelay = 0.25f;
+    private float jumpTimer;
+
     [Header("Components")]
     public Rigidbody2D rb;
+    public LayerMask groundLayer;
+    
 
     [Header("Physics")]
     public float maxSpeed = 7f;
     public float linerDrag = 4f;
+    public float gravity = 1;
+    public float fallMultiplier = 5f;
+
+    [Header("Collision")]
+    public bool onGround = false;
+    public float groundLength = 0.6f;
 
     private void Update()
     {
+        onGround = Physics2D.Raycast(transform.position, Vector2.down, groundLength, groundLayer);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpTimer = Time.time + jumpDelay;
+        }
+
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
     private void FixedUpdate()
     {
         moveCharacter(direction.x);
+
+        if (jumpTimer > Time.time && onGround)
+        {
+            Jump();
+        }
+
         modifyPhysics();
     }
 
@@ -34,17 +60,49 @@ public class burgier_movement : MonoBehaviour
         }
     }
 
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse); // Pierwszy parametr kierunek i moc, drugi parametr oznacza ze sie robi to instant jak zupka
+        jumpTimer = 0;
+    }
+
     void modifyPhysics()
     {
         bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
 
-        if(Mathf.Abs(direction.x) < 0.4f || changingDirections)
+        if (onGround)
         {
-            rb.drag = linerDrag;
+            if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+            {
+                rb.drag = linerDrag;
+            }
+            else
+            {
+                rb.drag = 0f;
+            }
+            rb.gravityScale = 0;
         }
         else
         {
-            rb.drag = 0f;
+            rb.gravityScale = gravity;
+            rb.drag = linerDrag * 0.15f;
+            
+            if(rb.velocity.y < 0)
+            {
+                rb.gravityScale = gravity * fallMultiplier;
+            } else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.gravityScale = gravity * (fallMultiplier / 2);
+            }
         }
+
+  
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundLength);
     }
 }
