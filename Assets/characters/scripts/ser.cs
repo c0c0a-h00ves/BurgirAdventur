5f5b6ededc258d06f65629b83a5c93802f5c63b8
xMikuuu <m.polatynski9@gmail.com>
2022-02-25 17:30:43 +0100

    Ser skończony prawie
    giga

--- Assets/characters/scripts/ser.cs
+++ Assets/characters/scripts/ser.cs
@@ -5,30 +5,122 @@ using UnityEngine;
 public class ser : MonoBehaviour
 {
     private GameObject burger;
-    public bool cheesed;
+    private BoxCollider2D boxCollider2d;
+    private GameObject spermokula;
+    private GameObject level;
 
+
+
+    private Vector3 initialPosition;
+    private Vector3 initialPosition2;
+
+    public bool cheesed;
+    private bool isSpawned;
+    public bool isLaunched;
+
+    private float facing_direction;
+    private float launchTime;
+
+    //  [SerializeField] float timeUntilDestruction = 5;
+    [SerializeField] float spawnDistance = 0.1f;
+    [SerializeField] float launchPower = 25;
+    [SerializeField] float maxlaunchTime = 3;
+
+    
     private void Awake()
     {
         burger = GameObject.Find("PlayerPrefab");
+        spermokula = GameObject.Find("spermokula");
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        level = GameObject.Find("Level1-1");
+        initialPosition2 = transform.position;
+        initialPosition = spermokula.transform.position;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         if (collision.gameObject == burger && collision.contacts[0].normal.x < -0.5 || collision.gameObject == burger && collision.contacts[0].normal.x > 0.5)
         {
             cheesed = true;
-            Debug.Log("chuj");
-            burger.GetComponent<Rigidbody2D>().gravityScale = 0;
-            Debug.Log(burger.GetComponent<Rigidbody2D>().gravityScale);
         }
     }
 
+
+
+
+
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject == burger) 
-        { 
-            cheesed = false; 
+        if (collision.gameObject == burger)
+        {
+            cheesed = false;
         }
     }
+
+
+    private void Update()
+    {
+        // RaycastHit2D raycastHit2d = Physics2D.Raycast(burger.transform.position, new Vector2(0, 1), 10);
+        // Debug.DrawRay(burger.transform.position, Vector2.right, Color.green);
+
+        if (Input.GetAxisRaw("Horizontal") != 0f)
+        {
+            facing_direction = Input.GetAxisRaw("Horizontal");
+        }
+
+
+        if (isLaunched)
+        { // sprawdzanie czy wolno sie porusza i czy zresetowac mape
+            launchTime += Time.deltaTime;
+        }
+
+        if (launchTime > maxlaunchTime)
+        {
+            spermokula.transform.position = initialPosition2;
+            spermokula.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0);
+
+            isLaunched = false;
+            launchTime = 0;
+        }
+
+
+        //Debug.Log(facing_direction);
+
+        if (facing_direction == -1)
+        {
+            RaycastHit2D raycastHit2d = Physics2D.Raycast(burger.transform.position, new Vector2(-5, 3));
+            Debug.DrawRay(burger.transform.position, new Vector2(-5, 3), Color.green);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                launchTime = 0;
+                spermokula.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0);
+                spermokula.transform.position = new Vector3(burger.transform.position.x + (spawnDistance * facing_direction), burger.transform.position.y, 0);
+
+
+                Vector2 directionToInitialPosition = initialPosition - (burger.transform.position - new Vector3(-5, 3));
+                spermokula.GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * launchPower);
+                isLaunched = true;
+            }
+        }
+
+        if (facing_direction == 1)
+        {
+            RaycastHit2D raycastHit2d = Physics2D.Raycast(burger.transform.position, new Vector2(5, 3));
+            Debug.DrawRay(burger.transform.position, new Vector2(5, 3), Color.green);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                launchTime = 0;
+                spermokula.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0);
+                spermokula.transform.position = new Vector3(burger.transform.position.x + (spawnDistance * facing_direction), burger.transform.position.y, 0);
+
+
+                initialPosition = spermokula.transform.position;
+                Vector2 directionToInitialPosition = initialPosition + (burger.transform.position + new Vector3(5, 3));
+                spermokula.GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * launchPower);
+                isLaunched = true;
+            }
+        }
+    }
+
 }
---
+++ Assets/characters/scripts/spermokula.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spermokula : MonoBehaviour
+{
+    private GameObject burger;
+    private GameObject klocek;
+    private GameObject ser;
+    private GameObject level;
+
+    private BoxCollider2D boxCollider2d;
+
+
+    private Vector3 initialPosition;
+    private Vector3 initialPosition2;
+    private Vector3 initialPosition3;
+
+    private float wielkosci;
+    private float iks;
+    private Collider2D colliderSera;
+    private Vector3 sizeSera;
+    private float iks2;
+    private bool przyklejone;
+
+
+    private float czas;
+    [SerializeField] private float maxCzas = 5f;
+
+    private Collider2D colliderKlocka;
+    private Vector3 sizeKlocka;
+
+    private void Awake()
+    {
+        burger = GameObject.Find("PlayerPrefab");
+        ser = GameObject.Find("ser");
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        level = GameObject.Find("Level1-1");
+        klocek = GameObject.Find("klocek");
+        initialPosition = ser.transform.position;
+        initialPosition2 = transform.position;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject == ser && collision.contacts[0].normal.x < -0.5 || collision.gameObject == ser && collision.contacts[0].normal.x > 0.5)
+        {
+            Debug.Log("ser");
+            ser.GetComponent<ser>().isLaunched = false;
+            czas = 0;
+            transform.position = initialPosition2;
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0);
+        }
+
+
+            if (collision.gameObject == klocek && collision.contacts[0].normal.x < -0.5 || collision.gameObject == klocek && collision.contacts[0].normal.x > 0.5) 
+        {
+           // wielkosci = klocek.GetComponent<Transform>().localScale.x;
+           // iks = wielkosci / 2;
+            Debug.Log(wielkosci);
+            Debug.Log("sciana");
+            initialPosition3 = transform.position;
+            transform.position = initialPosition2;
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0);
+
+
+            colliderKlocka = klocek.GetComponent<Collider2D>();
+            sizeKlocka = colliderKlocka.bounds.size;
+            iks = sizeKlocka.x/2;
+
+            colliderSera = ser.GetComponent<Collider2D>();
+            sizeSera = colliderSera.bounds.size;
+            iks2 = sizeSera.x / 2;
+
+
+            if (collision.contacts[0].normal.x < -0.5)
+            {
+                ser.transform.position = new Vector2(klocek.transform.position.x - iks + iks2 - 0.005f, initialPosition3.y);
+            }
+            if (collision.contacts[0].normal.x > 0.5)
+            {
+                ser.transform.position = new Vector2(klocek.transform.position.x + iks - iks2 + 0.005f, initialPosition3.y);
+            }
+
+
+
+
+            ser.GetComponent<ser>().isLaunched = false;
+            przyklejone = true;
+            czas = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (przyklejone)
+        { // sprawdzanie czy wolno sie porusza i czy zresetowac mape
+            czas += Time.deltaTime;
+            if (czas >= maxCzas)
+            {
+                ser.transform.position = initialPosition;
+                
+                czas = 0f;
+                przyklejone = false;
+
+            }
+        }
+    }
+}
